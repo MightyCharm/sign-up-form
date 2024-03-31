eebb4c20ec799b514ef4bd72a603855c46414bb8
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-04-01 00:40:53 +0200

    Refactor code and improve design

--- script.js
+++ script.js
@@ -12,10 +12,13 @@ let passwordConfirm = "";
 console.log(btnSubmit);
 
 btnSubmit.addEventListener("click", (e) => {
-    console.log(e);
-    console.log("click");
-    if(password != passwordConfirm && password.length != 0 && passwordConfirm.length != 0) {
+    console.log("password: " + password);
+    console.log("passwordConfirm: " + passwordConfirm);
+    if(password != passwordConfirm && password.length >= 5 && passwordConfirm.length >= 5) {
+        console.log("here");
         e.preventDefault();
+
+
         divError.textContent = "Passwords don't match";
         divError.style.color = "#ff0000";
         inputPasswordConfirm.style.border = "1px solid red";
@@ -24,21 +27,19 @@ btnSubmit.addEventListener("click", (e) => {
     
 });
 
-// password is checked if user uses the second input (confirmPassword)
-inputPasswordConfirm.addEventListener("input", (e) => {
-    passwordConfirm += e.data;
-    divError.textContent = "";
-    inputPasswordConfirm.style.border = "1px solid #E5E7EB";
-    inputPassword.style.border = "1px solid #E5E7EB";
-    //console.log(e.data);
-});
-
 inputPassword.addEventListener("input", (e) => {
-    password += e.data;
+    password = inputPassword.value;
     divError.textContent = "";
     inputPasswordConfirm.style.border = "1px solid #E5E7EB";
     inputPassword.style.border = "1px solid #E5E7EB";
     //console.log(pw);
 });
 
-
+// password is checked if user uses the second input (confirmPassword)
+inputPasswordConfirm.addEventListener("input", (e) => {
+    passwordConfirm = inputPasswordConfirm.value;
+    divError.textContent = "";
+    inputPasswordConfirm.style.border = "1px solid #E5E7EB";
+    inputPassword.style.border = "1px solid #E5E7EB";
+    //console.log(e.data);
+});
