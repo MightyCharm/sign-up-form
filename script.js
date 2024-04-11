a31462e7774db8bc6eecfbd4ca94062250887fb6
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-04-12 00:17:42 +0200

    Clean up code and add credit for bg-image

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// goal compare text input for password and confirm password
+
 const btnSubmit = document.querySelector("#submit");
 const inputPassword = document.querySelector("#password");
 const inputPasswordConfirm = document.querySelector("#confirmPassword");
 const divValidate = document.querySelector(".validate");
+const borderMatch = "1px solid #00b33c";
+const borderNoMatch = "1px solid #ff0000";
+const borderNormal = "1px solid #E5E7EB";
 
 
 function validatePassword() {
@@ -10,40 +13,38 @@ function validatePassword() {
         inputPassword.value.length != 0 ) {
         divValidate.textContent = "* Passwords match";
         divValidate.style.color = "#00b33c";
-        inputPassword.style.border = "1px solid #00b33c";
+        inputPassword.style.border = borderMatch;
         inputPasswordConfirm.style.border = "1px solid #00b33c";
     }
     else {
         divValidate.textContent = "* Password don't match";
         divValidate.style.color = "#ff0000";
-        inputPassword.style.border = "1px solid #ff0000";
+        inputPassword.style.border = borderNoMatch;
         inputPasswordConfirm.style.border = "1px solid #ff0000";
     }
 }
 
+function setNormal() {
+    divValidate.textContent = "";
+    inputPassword.style.border = borderNormal;
+    inputPasswordConfirm.style.border = borderNormal;
+}
+
 inputPassword.addEventListener("keyup", (e) => {
-   
     if(inputPasswordConfirm.value.length != 0) {
         validatePassword();
     }
-    
     if(inputPassword.value.length == 0 && inputPasswordConfirm.value.length == 0) {
-        divValidate.textContent = "";
-        inputPassword.style.border = "1px solid #E5E7EB";
-        inputPasswordConfirm.style.border = "1px solid #E5E7EB";
+       setNormal();
     }
-    
 })
 
 inputPasswordConfirm.addEventListener("keyup", () => {
     validatePassword();
     if(inputPassword.value.length == 0 && inputPasswordConfirm.value.length == 0) {
-        divValidate.textContent = "";
-        inputPassword.style.border = "1px solid #E5E7EB";
-        inputPasswordConfirm.style.border = "1px solid #E5E7EB";
+        setNormal();
     }
 })
-
 
 btnSubmit.addEventListener("click", (e) => {
     if(inputPassword.value != inputPasswordConfirm.value) {
